b29532023a7a0f73e7fc523026cd62e4265399d6
dkim61 <dkim187@binghamton.edu>
2022-06-23 22:26:34 -0400

    fixed warnings

--- tweetz-web/src/index.js
+++ tweetz-web/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { TweetzComponent } from './tweetz';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+
 
 const appEl = document.getElementById('root');
 if (appEl){
-  ReactDOM.render(<App />, appEl);
+  const rootAppEl = createRoot(appEl)
+  rootAppEl.render(<App/>)
 }
 
 const tweetsEl = document.getElementById("tweetme")
 if (tweetsEl){
-  ReactDOM.render(<TweetzComponent />, tweetsEl);
+  const rootTweetEl = createRoot(tweetsEl)
+  rootTweetEl.render(<TweetzComponent/>)
 }
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
